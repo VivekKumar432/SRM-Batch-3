f38bc2058a654ca2bbd1347d52b46fd0c4621393
NIMISHAM2305 <152949153+NIMISHAM2305@users.noreply.github.com>
2024-07-06 22:07:25 +0530

    editted file

--- src/app.js
+++ src/app.js
@@ -3,11 +3,11 @@ const signupRoute = require("./routes/Signup");
 const bodyParser = require("body-parser");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use("/user", signupRoute);
 
 app.listen(PORT, () => {
-    console.log('server is running on http://localhost:${PORT}');
+    console.log('server is running on http://localhost:3000');
 })
