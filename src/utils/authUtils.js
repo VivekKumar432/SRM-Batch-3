1269e2e4f2b63e6fc7bda791b08d942951cda150
NIMISHAM2305 <152949153+NIMISHAM2305@users.noreply.github.com>
2024-07-07 04:07:02 +0530

    jwt config and auth utils

---
+++ src/utils/authUtils.js
@@ -0,0 +1,26 @@
+const jwt = require("jsonwebtoken");
+const secretKey = require("../configuration/jwtConfig");
+
+function generateToken(user) {
+    const payload = {
+        id: user._id,
+        email: user.email,
+        role: user.role
+    };
+    return jwt.sign(payload, secretKey, { expiresIn: "1h" });
+};
+
+function generateRefreshToken(user) {
+    const payload = {
+        id: user._id,
+        email: user.email,
+        role: user.role
+    };
+    return jwt.sign(payload, secretKey, { expiresIn: "7h" });
+};
+
+function verifyToken(token) {
+    return jwt.verify(token, secretKey);
+}
+
+module.exports = { generateToken, generateRefreshToken, verifyToken };
