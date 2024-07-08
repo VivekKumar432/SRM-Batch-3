b7224f102196762d484be881388d4970ac8ca6ef
Saloni-singh22 <saloni.singh2428@gmail.com>
2024-07-08 15:33:39 +0530

    latest commit

---
+++ frontend/src/components/AdminHome.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './AdminHome.css';
+
+function AdminHome() {
+  return (
+    <div className="container">
+      <h1>Welcome, Admin!</h1>
+      <p>This is the admin home screen.</p>
+    </div>
+  );
+}
+
+export default AdminHome;
