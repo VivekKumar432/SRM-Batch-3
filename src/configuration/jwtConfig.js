96c87d52b982be40f8622eb2a94df603fd044389
Vivek Kumar <piebytwo014@gmail.com>
2024-07-15 10:59:54 +0530

    refactor code

--- src/configuration/jwtConfig.js
+++
@@ -1,3 +0,0 @@
-const secretKey = 'G@2#4fa5!E%P0KqwJH7^&8lbRvZu1DxsY9eTI+c3VN6o-@mLrBjW'
-
-module.exports = { secretKey: secretKey };
--- src/routes/Login.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { login } = require("../controller/Login");
-
-const router = express.Router();
-
-router.post("/login", login);
-
-module.exports = router;
